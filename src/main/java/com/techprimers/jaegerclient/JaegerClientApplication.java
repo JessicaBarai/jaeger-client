77aa39d77b11b63c6bc22931f33b0c8816b98f98
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-02 10:59:13 +0200

    Update JaegerClientApplication.java

--- src/main/java/com/techprimers/jaegerclient/JaegerClientApplication.java
+++ src/main/java/com/techprimers/jaegerclient/JaegerClientApplication.java
@@ -4,7 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class JaegerClientApplication {
+	
+	@GetMapping("/")
+	public String welcome() {
+		System.out.print("change enabled");
+		return "welcome to java!";
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JaegerClientApplication.class, args);
