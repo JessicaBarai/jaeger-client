3237333dae2d67cf4fbc5606f175df261e092be2
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-02 11:07:01 +0200

    Update JaegerClientApplication.java

--- src/main/java/com/techprimers/jaegerclient/JaegerClientApplication.java
+++ src/main/java/com/techprimers/jaegerclient/JaegerClientApplication.java
@@ -15,6 +15,7 @@ public class JaegerClientApplication {
 	}
 
 	public static void main(String[] args) {
+		System.out.print("STARTING WITH PROPERTIES CHANGED!");
 		SpringApplication.run(JaegerClientApplication.class, args);
 	}
 
