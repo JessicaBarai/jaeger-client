a7f4b9ffddd7b651b97a924568118523904b5f7a
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-04 17:44:58 +0200

    Update JaegerClientService.java

--- src/main/java/com/techprimers/jaegerclient/service/JaegerClientService.java
+++ src/main/java/com/techprimers/jaegerclient/service/JaegerClientService.java
@@ -16,7 +16,7 @@ public class JaegerClientService {
   public Mono<String> get(Integer id) {
 
     return webClient.get()
-        .uri("https://jaeger-server-git-edb.apps.aro.euw-hub03.azure.volvo.net/jaeger/server/" + id)
+        .uri("http://numbersapi.com/" + id)
         .retrieve()
         .bodyToMono(String.class);
   }
