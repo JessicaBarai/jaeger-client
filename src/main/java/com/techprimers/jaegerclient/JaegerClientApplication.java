b8f6861b45883e2b37d96fc29bc6f8df4c90819e
JessicaBarai <81428391+JessicaBarai@users.noreply.github.com>
2021-07-05 10:54:24 +0200

    Update JaegerClientApplication.java

--- src/main/java/com/techprimers/jaegerclient/JaegerClientApplication.java
+++ src/main/java/com/techprimers/jaegerclient/JaegerClientApplication.java
@@ -1,23 +1,42 @@
 package com.techprimers.jaegerclient;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class JaegerClientApplication {
 	
-	@GetMapping("/")
-	public String welcome() {
-		System.out.print("change enabled");
-		return "Hello from Client!";
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+		
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	@RequestMapping("/")
+	public String welcome() throws JSONException {
+		JSONObject jsonObject= new JSONObject();
+		jsonObject.put("Message", "Hello from client");
+		jsonObject.put("Message from server to client", restTemplate.exchange(
+				"http://localhost:8082/", HttpMethod.GET, null, String.class).getBody());
+		return jsonObject.toString();
 		
 	}
 
 	public static void main(String[] args) {
-		System.out.print("STARTING WITH PROPERTIES CHANGED1.2!");
+		System.out.print("STARTING WITH PROPERTIES CHANGED!");
 		SpringApplication.run(JaegerClientApplication.class, args);
 	}
 
